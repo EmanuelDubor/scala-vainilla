1669433cdf7bd4c4be58e669039469527d1414d1
EmanuelDubor <emanueldubora@hotmail.com>
2016-04-03 19:57:00 -0300

    Initial commit

---
+++ desktop/src/edu/unq/vainilla/arkanoid/desktop/DesktopLauncher.scala
@@ -0,0 +1,14 @@
+package edu.unq.vainilla.arkanoid.desktop
+
+import com.badlogic.gdx.backends.lwjgl.{LwjglApplication, LwjglApplicationConfiguration}
+import edu.unq.vainilla.arkanoid.ArkanoidDemo
+
+object DesktopLauncher {
+
+  def main(args: Array[String]) {
+    val config = new LwjglApplicationConfiguration
+    new LwjglApplication(new ArkanoidDemo(), config)
+  }
+
+
+}
